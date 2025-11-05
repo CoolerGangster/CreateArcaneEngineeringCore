7ed09b737fb7566dc3bd9d897e025f8bdc5f2eaf
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2025-11-05 18:48:28 +0100

    Added a much needed null safety check (the game would crash if you had no quest chapter selected (possible if there are no chapters))

--- src/main/java/com/pino/cae/mixin/FTBQuestScreenBackgroundMixin.java
+++ src/main/java/com/pino/cae/mixin/FTBQuestScreenBackgroundMixin.java
@@ -6,6 +6,7 @@ import dev.ftb.mods.ftblibrary.ui.BaseScreen;
 import dev.ftb.mods.ftblibrary.ui.Theme;
 import dev.ftb.mods.ftbquests.gui.quests.QuestScreen;
 import dev.ftb.mods.ftbquests.quest.Chapter;
+import dev.ftb.mods.ftbquests.quest.theme.property.ThemeProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,6 +31,10 @@ public abstract class FTBQuestScreenBackgroundMixin {
             )
     )
     private void doNotDrawDefaultSquareBackground(BaseScreen instance, PoseStack matrixStack, Theme theme, int x, int y, int w, int h) {
+        if (this.selectedChapter == null) {
+            ThemeProperties.BACKGROUND.get().draw(matrixStack, x, y, w, h);
+            return;
+        }
         // is this cursed?
         ((INowHaveABackgroundImage) (Object) this.selectedChapter).cae_getBackgroundImage().draw(matrixStack, x, y, w, h);
     }
