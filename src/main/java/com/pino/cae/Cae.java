1fe7e73db52ff35f26d330d557310d526cb57b8e
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2025-11-05 15:22:05 +0100

    Fixed Maven dependencies + added FTB mods

--- src/main/java/com/pino/cae/Cae.java
+++ src/main/java/com/pino/cae/Cae.java
@@ -1,7 +1,6 @@
 package com.pino.cae;
 
 import com.mojang.logging.LogUtils;
-import com.pino.cae.events.forgebus;
 import com.pino.cae.init.FluidInit;
 import com.pino.cae.init.ModEntityTypes;
 import com.pino.cae.entity.OrbRender;
@@ -10,35 +9,23 @@ import com.pino.cae.init.ItemInit;
 import com.pino.cae.init.blockEntity.ModBlockEntities;
 import com.pino.cae.sounds.soundinit;
 import com.simibubi.create.foundation.data.CreateRegistrate;
-import com.tterrag.registrate.Registrate;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistry;
-import org.intellij.lang.annotations.Identifier;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
-import slimeknights.mantle.registration.RegistrationHelper;
 import software.bernie.geckolib3.GeckoLib;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
-import java.util.AbstractMap;
 import java.util.HashMap;
 
 // The value here should match an entry in the META-INF/mods.toml file
